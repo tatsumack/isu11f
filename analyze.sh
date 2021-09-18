5702f6fec8057f85f8faa12f1f6ba449ffe4c5cc
naoya.tabakomori <cigaret.cigaret@gmail.com>
2021-09-18 11:32:15 +0900

    analyzeを追加

--- analyze.sh
+++ analyze.sh
@@ -8,10 +8,13 @@ mkdir -p ${LOG_DIR}
 if [ -f ${LOG_DIR}/alp.log ]; then
     sudo mv ${LOG_DIR}/alp.log ${LOG_DIR}/alp.log.$(date "+%Y%m%d_%H%M%S")
 fi
-sudo /usr/local/bin/alp -f /var/log/nginx/access.log --sum -r > ${LOG_DIR}/alp.log
+sudo /usr/local/bin/alp -f /var/log/nginx/access.log --sum -r --aggregates='/api/courses/[A-Z0-9]+/classes$,/api/courses/[A-Z0-9]+/classes/[A-Z0-9]+/assignments$,/api/announcements/[A-Z0-9]+$,/api/courses/[A-Z0-9]+/classes/[A-Z0-9]+/assignments/export$,/api/courses/[A-Z0-9]+/classes/[A-Z0-9]+/assignments/scores$,/api/courses/[A-Z0-9]+$,/api/courses/[A-Z0-9]+/status$' > ${LOG_DIR}/alp.log
+
+# sudo /usr/local/bin/alp -f /var/log/nginx/access.log --sum -r --aggregates='/api/courses/[A-Z0-9]+/classes$,/api/courses/[A-Z0-9]+/classes/[A-Z0-9]+/assignments$,/api/announcements/[A-Z0-9]+,/api/courses/[A-Z0-9]+/classes/[A-Z0-9]+/assignments/export,/api/courses/[A-Z0-9]+/classes/[A-Z0-9]+/assignments/scores,/api/courses/[A-Z0-9]+/status,/api/courses/[A-Z0-9]+' > ${LOG_DIR}/alp.log
+
 
 # slow query
-if [ -f ${LOG_DIR}/mysql-slow.log ]; then
-    sudo mv ${LOG_DIR}/mysql-slow.log ${LOG_DIR}/mysql-slow.log.$(date "+%Y%m%d_%H%M%S")
+if [ -f ${LOG_DIR}/mysql-slow-query.log ]; then
+    sudo mv ${LOG_DIR}/mysql-slow-query.log ${LOG_DIR}/mysql-slow-query.log.$(date "+%Y%m%d_%H%M%S")
 fi
-sudo /usr/local/bin/pt-query-digest /var/log/mysql/mysql-slow.log > ${LOG_DIR}/mysql-slow.log
+sudo /usr/local/bin/pt-query-digest /var/log/mysql/mysql-slow.log > ${LOG_DIR}/mysql-slow-query.log
